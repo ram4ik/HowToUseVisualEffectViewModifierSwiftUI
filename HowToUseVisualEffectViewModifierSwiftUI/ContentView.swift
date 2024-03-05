c12d3d2874406f08c3cb59c5d430683bd4e7dbcb
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-03-05 20:06:55 +0200

    withAnimation

--- HowToUseVisualEffectViewModifierSwiftUI/ContentView.swift
+++ HowToUseVisualEffectViewModifierSwiftUI/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showSpacer: Bool = false
+    
     var body: some View {
-        Text("Visual Effect")
-            .padding()
-            .background(.red)
-            .visualEffect { content, geometry in
-                content
-                    .grayscale(geometry.size.width >= 200 ? 1 : 0)
+        VStack {
+            Text("Visual Effect")
+                .padding()
+                .background(.red)
+                .visualEffect { content, geometry in
+                    content
+                        .grayscale(geometry.frame(in: .global).minY < 300 ? 1 : 0)
+                }
+            
+            if showSpacer {
+                Spacer()
             }
+        }
+        .animation(.easeIn, value: showSpacer)
+        .onTapGesture {
+            showSpacer.toggle()
+        }
     }
 }
 
