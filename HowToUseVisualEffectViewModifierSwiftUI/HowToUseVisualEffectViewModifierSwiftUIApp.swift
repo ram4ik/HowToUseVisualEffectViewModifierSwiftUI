ecf98f6ce5016fb4cc928639d7c9c8883ea71b63
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-03-05 19:51:33 +0200

    Initial Commit

---
+++ HowToUseVisualEffectViewModifierSwiftUI/HowToUseVisualEffectViewModifierSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToUseVisualEffectViewModifierSwiftUIApp.swift
+//  HowToUseVisualEffectViewModifierSwiftUI
+//
+//  Created by test on 05.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct HowToUseVisualEffectViewModifierSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
